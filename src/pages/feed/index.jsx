7ded7ae8b8ea27c388ee3ab554a15cd7f8edcaad
Geovane Nascimento <67743864+geovane8@users.noreply.github.com>
2023-05-11 18:46:07 -0300

    Update index.jsx

--- src/pages/feed/index.jsx
+++ src/pages/feed/index.jsx
@@ -1,39 +1,36 @@
-import React from 'react'
+import React from 'react';
 import { Card } from '../../components/Card';
 import { UserInfo } from '../../components/UserInfo';
-
 import { Header } from '../../components/Header';
-
 import { Container, Column, Title, TitleHighlight } from './styles';
+
+const cardsData = Array.from({ length: 10 }, (_, index) => <Card key={index} />);
+
+const rankingData = [
+  { nome: 'Pablo Henrique', image: 'https://avatars.githubusercontent.com/u/45184516?v=', percentual: 25 },
+  { nome: 'Pablo Henrique', image: 'https://avatars.githubusercontent.com/u/45184516?v=', percentual: 65 },
+  { nome: 'Pablo Henrique', image: 'https://avatars.githubusercontent.com/u/45184516?v=', percentual: 45 },
+  { nome: 'Pablo Henrique', image: 'https://avatars.githubusercontent.com/u/45184516?v=', percentual: 72 },
+];
 
 const Feed = () => {
   return (
     <>
-        <Header autenticado={true}/>
-        <Container>
-            <Column flex={3}>
-                <Title>Feed</Title>
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-            </Column>
-            <Column flex={1}>
-              <TitleHighlight> # RANKING 5 TOP DA SEMANA </TitleHighlight>
-                <UserInfo nome="Pablo Henrique" image="https://avatars.githubusercontent.com/u/45184516?v=" percentual={25}/>
-                <UserInfo nome="Pablo Henrique" image="https://avatars.githubusercontent.com/u/45184516?v=" percentual={65}/>
-                <UserInfo nome="Pablo Henrique" image="https://avatars.githubusercontent.com/u/45184516?v=" percentual={45}/>
-                <UserInfo nome="Pablo Henrique" image="https://avatars.githubusercontent.com/u/45184516?v=" percentual={72}/>
-            </Column>
-        </Container>
+      <Header autenticado={true} />
+      <Container>
+        <Column flex={3}>
+          <Title>Feed</Title>
+          {cardsData}
+        </Column>
+        <Column flex={1}>
+          <TitleHighlight># RANKING 5 TOP DA SEMANA</TitleHighlight>
+          {rankingData.map(({ nome, image, percentual }, index) => (
+            <UserInfo key={index} nome={nome} image={image} percentual={percentual} />
+          ))}
+        </Column>
+      </Container>
     </>
-  )
-}
+  );
+};
 
-export { Feed }; 
+export { Feed };
