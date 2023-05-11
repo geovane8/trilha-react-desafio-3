13858fe0b70c751a59144f06cf556db17ce5446f
Geovane Nascimento <67743864+geovane8@users.noreply.github.com>
2023-05-11 18:41:54 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import { Feed } from "./pages/feed";
-
-import { Home } from './pages/home'
-import { Login } from './pages/login'
-import { GlobalStyle } from './styles/global';
+import { Home } from "./pages/home";
+import { Login } from "./pages/login";
+import { GlobalStyle } from "./styles/global";
 
 function App() {
   return (
     <Router>
-     <GlobalStyle />
-     <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/feed" element={<Feed />} />
-     </Routes >
+      <GlobalStyle />
+      <Switch>
+        <Route exact path="/">
+          <Redirect to="/home" />
+        </Route>
+        <Route path="/home">
+          <Home />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/feed/:id">
+          <Feed />
+        </Route>
+        <Route path="*">
+          <Redirect to="/home" />
+        </Route>
+      </Switch>
     </Router>
   );
 }
